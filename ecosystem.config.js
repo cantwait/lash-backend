d4aa46a40e7b61a25fbfd0327a11ebb6ba37e3ce
Rafael Cadenas <cadenas.rafael@gmail.com>
2018-02-18 04:15:19 -0500

    setup pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'lash',
+    script: './app.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-218-41-176.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/lash-server.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:cantwait/lash-backend.git',
+      path: '/home/ubuntu/lash-backend',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
